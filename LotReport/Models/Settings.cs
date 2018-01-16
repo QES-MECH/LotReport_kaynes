af32b6741445c3c720158790321891dacda6e3a3
Will <wailinnyu@gmail.com>
2018-01-16 18:09:25 +0800

    Added Settings

---
+++ LotReport/Models/Settings.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace LotReport.Models
+{
+    public static class Settings
+    {
+        public const string SettingsDirectory = "Settings.xml";
+
+        public static string DatabaseDirectory { get; set; }
+
+        public static string RejectCodesDirectory { get; set; }
+
+        public static void LoadFromFile()
+        {
+            XDocument document = XDocument.Load(SettingsDirectory);
+
+            DatabaseDirectory = document
+                .Root
+                .Element("General")
+                .Element("Directories")
+                .Element("Database")
+                .Value;
+
+            RejectCodesDirectory = document
+                .Root
+                .Element("General")
+                .Element("Directories")
+                .Element("RejectCodes")
+                .Value;
+        }
+
+        public static void SaveToFile()
+        {
+            XmlWriterSettings xmlSettings = new XmlWriterSettings();
+            xmlSettings.Indent = true;
+            xmlSettings.IndentChars = "  ";
+
+            using (XmlWriter writer = XmlWriter.Create(SettingsDirectory))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("General");
+
+                writer.WriteStartElement("Directories");
+                writer.WriteElementString("Database", DatabaseDirectory);
+                writer.WriteElementString("RejectCodes", RejectCodesDirectory);
+                writer.WriteEndElement();
+
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+            }
+        }
+    }
+}
---
+++ LotReport/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using LotReport.Models;
+using Microsoft.Win32;
+
+namespace LotReport.ViewModels
+{
+    public class SettingsViewModel : PropertyChangedBase
+    {
+        private string _databaseDirectory;
+        private string _rejectCodesDirectory;
+
+        public SettingsViewModel()
+        {
+            this.LoadData();
+            this.WireCommands();
+        }
+
+        public string DatabaseDirectory
+        {
+            get
+            {
+                return _databaseDirectory;
+            }
+
+            set
+            {
+                _databaseDirectory = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string RejectCodesDirectory
+        {
+            get
+            {
+                return _rejectCodesDirectory;
+            }
+
+            set
+            {
+                _rejectCodesDirectory = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand<bool> OkCommand { get; private set; }
+
+        public RelayCommand DatabaseFolderCommand { get; private set; }
+
+        public RelayCommand RejectCodesFileCommand { get; private set; }
+
+        private void WireCommands()
+        {
+            OkCommand = new RelayCommand<bool>(
+                param => SaveData());
+
+            DatabaseFolderCommand = new RelayCommand(
+                param =>
+                {
+                    using (var folderDialog = new System.Windows.Forms.FolderBrowserDialog())
+                    {
+                        folderDialog.Description = "Browse for Database Folder";
+                        folderDialog.RootFolder = Environment.SpecialFolder.Desktop;
+                        folderDialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                        var result = folderDialog.ShowDialog();
+
+                        if (result == System.Windows.Forms.DialogResult.OK)
+                        {
+                            DatabaseDirectory = folderDialog.SelectedPath;
+                        }
+                    }
+                });
+
+            RejectCodesFileCommand = new RelayCommand(
+                param =>
+                {
+                    OpenFileDialog fileDialog = new OpenFileDialog();
+                    fileDialog.Title = "Open RejectCodes";
+                    fileDialog.DefaultExt = ".xml";
+                    fileDialog.Filter = "(*.XML)|*.xml";
+                    bool? result = fileDialog.ShowDialog();
+
+                    if (result == true)
+                    {
+                        RejectCodesDirectory = fileDialog.FileName;
+                    }
+                });
+        }
+
+        private void LoadData()
+        {
+            DatabaseDirectory = Settings.DatabaseDirectory;
+            RejectCodesDirectory = Settings.RejectCodesDirectory;
+        }
+
+        private bool SaveData()
+        {
+            MessageBoxResult result = MessageBox.Show(
+                "Are you sure you want to save?",
+                "Save Settings",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question);
+            if (result != MessageBoxResult.Yes)
+            {
+                return false;
+            }
+
+            Settings.DatabaseDirectory = DatabaseDirectory;
+            Settings.RejectCodesDirectory = RejectCodesDirectory;
+
+            try
+            {
+                Settings.SaveToFile();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    string.Format("Failed to save Settings. Error: {0}", ex.Message),
+                    "Error",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
