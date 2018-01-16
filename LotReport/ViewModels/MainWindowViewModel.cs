b4d85ff085f7133589b04461cbb0c842199bb1cb
Will <wailinnyu@gmail.com>
2018-01-16 16:01:15 +0800

    Added MainWindowViewModel

---
+++ LotReport/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LotReport.Models;
+using LotReport.Models.DirectoryItems;
+
+namespace LotReport.ViewModels
+{
+    public class MainWindowViewModel : PropertyChangedBase
+    {
+        private string _status;
+        private LeadFrameTable _leadFrameMap;
+        private List<Item> _directoryItems;
+
+        public MainWindowViewModel()
+        {
+            this.WireCommands();
+        }
+
+        public string Status
+        {
+            get
+            {
+                return _status;
+            }
+
+            set
+            {
+                _status = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public LeadFrameTable LeadFrameMap
+        {
+            get
+            {
+                return _leadFrameMap;
+            }
+
+            set
+            {
+                _leadFrameMap = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public List<Item> DirectoryItems
+        {
+            get
+            {
+                return _directoryItems;
+            }
+
+            set
+            {
+                _directoryItems = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public AsyncCommand<object> LoadedCommand { get; private set; }
+
+        private void WireCommands()
+        {
+        }
+    }
+}
