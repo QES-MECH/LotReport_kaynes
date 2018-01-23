1c1159c1f14205f32a6833e3328eb1b22528f09f
Will <wailinnyu@gmail.com>
2018-01-23 16:30:03 +0800

    Added UnitTextBox reusable control

---
+++ LotReport/Views/ReusableControls/UnitTextBox.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LotReport.Views.ReusableControls
+{
+    /// <summary>
+    /// Interaction logic for UnitTextBox.xaml
+    /// </summary>
+    public partial class UnitTextBox : UserControl
+    {
+        // Using a DependencyProperty as the backing store for Label.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register("Label", typeof(string), typeof(UnitTextBox), new PropertyMetadata("Label: "));
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(string), typeof(UnitTextBox), new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        // Using a DependencyProperty as the backing store for Ratio.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelWidthProperty =
+            DependencyProperty.Register("LabelWidth", typeof(GridLength), typeof(UnitTextBox), new PropertyMetadata(new GridLength(0.7, GridUnitType.Star)));
+
+        // Using a DependencyProperty as the backing store for ValueWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ValueWidthProperty =
+            DependencyProperty.Register("ValueWidth", typeof(GridLength), typeof(UnitTextBox), new PropertyMetadata(new GridLength(1, GridUnitType.Star)));
+
+        // Using a DependencyProperty as the backing store for UnitWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UnitWidthProperty =
+            DependencyProperty.Register("UnitWidth", typeof(GridLength), typeof(UnitTextBox), new PropertyMetadata(new GridLength(0, GridUnitType.Auto)));
+
+        // Using a DependencyProperty as the backing store for LabelAlignment.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelAlignmentProperty =
+            DependencyProperty.Register("LabelAlignment", typeof(HorizontalAlignment), typeof(UnitTextBox), new PropertyMetadata(HorizontalAlignment.Right));
+
+        // Using a DependencyProperty as the backing store for TextAlignment.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ValueAlignmentProperty =
+            DependencyProperty.Register("ValueAlignment", typeof(TextAlignment), typeof(UnitTextBox), new PropertyMetadata(TextAlignment.Right));
+
+        // Using a DependencyProperty as the backing store for Unit.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UnitProperty =
+            DependencyProperty.Register("Unit", typeof(string), typeof(UnitTextBox), new PropertyMetadata("Unit"));
+
+        public UnitTextBox()
+        {
+            this.InitializeComponent();
+        }
+
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { this.SetValue(LabelProperty, value); }
+        }
+
+        public string Value
+        {
+            get { return (string)GetValue(ValueProperty); }
+            set { this.SetValue(ValueProperty, value); }
+        }
+
+        public GridLength LabelWidth
+        {
+            get { return (GridLength)GetValue(LabelWidthProperty); }
+            set { this.SetValue(LabelWidthProperty, value); }
+        }
+
+        public GridLength ValueWidth
+        {
+            get { return (GridLength)GetValue(ValueWidthProperty); }
+            set { this.SetValue(ValueWidthProperty, value); }
+        }
+
+        public HorizontalAlignment LabelAlignment
+        {
+            get { return (HorizontalAlignment)GetValue(LabelAlignmentProperty); }
+            set { this.SetValue(LabelAlignmentProperty, value); }
+        }
+
+        public TextAlignment ValueAlignment
+        {
+            get { return (TextAlignment)GetValue(ValueAlignmentProperty); }
+            set { this.SetValue(ValueAlignmentProperty, value); }
+        }
+
+        public GridLength UnitWidth
+        {
+            get { return (GridLength)GetValue(UnitWidthProperty); }
+            set { this.SetValue(UnitWidthProperty, value); }
+        }
+
+        public string Unit
+        {
+            get { return (string)GetValue(UnitProperty); }
+            set { this.SetValue(UnitProperty, value); }
+        }
+    }
+}
